13e96369838b3cbea56fc72e0afebb0c478980c6
andrewglass3 <andrewglass3@gmail.com>
2018-01-20 14:38:44 +0000

    create install.sh
    initial commit of semi automated installer script for Ubiquiti UNMS on Centos

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Update and install updated packages on system
+
+yum update -y
+
+# Install yum-plugins-core package to provide the commands to manage your yum repos
+
+yum -y install yum-plugins-core
+
+# Set up the Docker stable repo
+
+yum config-manager -y \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+
+# Install docker 
+
+yum install -y docker-ce
+
+#enable docker service at boot
+
+# Start docker
+
+systemctl start docker
+
+# Create docker group
+
+groupadd docker
+
+# add user to docker group
+
+usermod -aG docker $USER
+
+# Install UNMS
+
+curl -fsSL https://raw.githubusercontent.com/Ubiquiti-App/UNMS/master/install.sh > /tmp/unms_install.sh && sudo bash /tmp/unms_install.sh
